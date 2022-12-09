44aa1797602dc83535d2301bfbd132a778f6030e
ndricomputing <ndripros2019@gmail.com>
2022-12-08 21:41:36 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu Byte(s)\n", sizeof(char));
+	printf("size of an int: %lu Byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu Byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu Byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu Byte(s)\n", sizeof(float));
+	return (0);
+
+}
